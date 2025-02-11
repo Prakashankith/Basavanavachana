d502e7cb58901394efbf8c5dac6176a9139361b2
Prakashankith <sirankithbp1999@gmail.com>
2025-02-11 22:08:01 +0530

    HashRouter is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 
